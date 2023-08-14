ebd33417751ee7674bdf4bc233c1788b2d32e7c6
Paul Terry <paul.terry84@gmail.com>
2023-08-14 21:01:00 +0100

    added help and exit commands

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var options map[string]cliCommand = make(map[string]cliCommand)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	scanner := bufio.NewScanner(os.Stdin)
+	buildOptions()
+
+	for {
+		fmt.Print("pokedex > ")
+		scanner.Scan()
+
+		if command, ok := options[scanner.Text()]; ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Printf("error: %s", err.Error())
+				break
+			}
+		}
+	}
+}
+
+func buildOptions() {
+	options["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	}
+	options["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
+}
+
+func commandHelp() error {
+	fmt.Printf("\nWelcome to the Pokedex!\n")
+	fmt.Printf("Usage: \n")
+	fmt.Println()
+
+	for _, command := range options {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	fmt.Println()
+
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
 }
